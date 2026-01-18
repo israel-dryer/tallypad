1ee7f07856a7a7b55084e651dab4f62d498a941a
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 14:19:33 -0500

    add initial project files

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBssHH8y3h_0DXIuwtGU06IelSAG6xPX7E",
+    authDomain: "tallypad-48a1a.firebaseapp.com",
+    projectId: "tallypad-48a1a",
+    storageBucket: "tallypad-48a1a.firebasestorage.app",
+    messagingSenderId: "386398434951",
+    appId: "1:386398434951:web:9cbe9b16764d9c8e8b2a22",
+    measurementId: "G-YZWPZSVKHG"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyBssHH8y3h_0DXIuwtGU06IelSAG6xPX7E",
+    authDomain: "tallypad-48a1a.firebaseapp.com",
+    projectId: "tallypad-48a1a",
+    storageBucket: "tallypad-48a1a.firebasestorage.app",
+    messagingSenderId: "386398434951",
+    appId: "1:386398434951:web:9cbe9b16764d9c8e8b2a22",
+    measurementId: "G-YZWPZSVKHG"
+  }
 };
 
 /*
