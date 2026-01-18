d2007c744de7b902af3c660428dfa80d914add1e
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 09:37:56 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'tallypad',
+  webDir: 'www'
+};
+
+export default config;
