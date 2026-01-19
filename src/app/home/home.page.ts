ee81481c2dabc46f09ec30d28d57c668288e0a8f
Israel Dryer <israel.dryer@gmail.com>
2026-01-18 19:55:45 -0500

    update maskable icons to prevent cropping.

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import {
+  IonHeader,
+  IonToolbar,
+  IonTitle,
+  IonContent,
+  IonList,
+  IonItem,
+  IonLabel,
+  IonIcon,
+  IonNote,
+  IonAvatar,
+  IonRippleEffect
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { chevronForward } from 'ionicons/icons';
+
+interface LauncherApp {
+  id: string;
+  name: string;
+  description: string;
+  icon: string;
+  url: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonIcon,
+    IonNote,
+    IonAvatar,
+    IonRippleEffect
+  ],
 })
 export class HomePage {
-  constructor() {}
+  apps: LauncherApp[] = [
+    {
+      id: 'catan-dice',
+      name: 'Catan Dice',
+      description: 'Dice roller and stats tracker for Catan',
+      icon: 'assets/launcher-icons/catan-dice.png',
+      url: 'https://catandice.tallypad.net'
+    }
+  ];
+
+  constructor() {
+    addIcons({ chevronForward });
+  }
+
+  launchApp(app: LauncherApp): void {
+    window.location.href = app.url;
+  }
 }
--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,7 @@ const analytics = getAnalytics(firebaseApp);
 
 bootstrapApplication(AppComponent, {
   providers: [
-    {provide: APP_VERSION, useValue: '0.1.1'},
+    {provide: APP_VERSION, useValue: '0.2.8'},
     { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
     provideIonicAngular(),
     provideRouter(routes, withPreloading(PreloadAllModules)),
